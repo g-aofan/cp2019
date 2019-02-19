15b5785dbf42f652ac9d47471790dc59918752e4
g-aofan <47775231+g-aofan@users.noreply.github.com>
2019-02-19 17:06:13 +0800

    Create q0_bmi.py

---
+++ p01/q0_bmi.py
@@ -0,0 +1,16 @@
+#get input
+weight= int(input("Enter weight(kg):"))
+height= float(input("Enter height(m):"))
+#compute bmi
+bmi= weight/(height*height)
+#output result
+print("bmi={0:.2f}".format(bmi))
+if bmi<18.5:
+    print("Please eat more")
+elif 18.5<=bmi<23:
+    print("Low risk")
+elif 23<=bmi<27:
+    print("Moderate risk")
+else:#>=27.5:
+    print("High Risk")
+    
