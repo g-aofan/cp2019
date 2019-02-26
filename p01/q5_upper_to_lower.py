7200311dd030b5dea2db08d75c42b6d2eddd49e7
g-aofan <47775231+g-aofan@users.noreply.github.com>
2019-02-26 17:08:30 +0800

    Update q5_upper_to_lower.py

--- p01/q5_upper_to_lower.py
+++ p01/q5_upper_to_lower.py
@@ -1,3 +1,3 @@
-uppercase_letter = input('Input capital letter here ')
+uppercase_letter = input('Input capital letter here: ')
 lowercase_letter = chr(ord(uppercase_letter) + 32)
 print(lowercase_letter)    
