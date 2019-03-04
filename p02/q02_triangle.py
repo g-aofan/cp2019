988b67ad834c245f2c52f4a6c77ef611a63c4f85
g-aofan <47775231+g-aofan@users.noreply.github.com>
2019-03-04 21:17:24 +0800

    Update q02_triangle.py

--- p02/q02_triangle.py
+++ p02/q02_triangle.py
@@ -1,1 +1,8 @@
+s1 = int(input("Enter side 1: "))
+s2 = int(input("Enter side 2: "))
+s3 = int(input("Enter side 3: "))
 
+if s1 + s2 > s3 and s1 + s3 > s2 and s2 + s1 > s3:
+    print("Perimeter = ","{0}".format(s1 + s2 + s3))
+else:
+    print("Invalid triangle!!")
