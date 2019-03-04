478d1808dff7c126d3da808417bb2f53653b2cb6
g-aofan <47775231+g-aofan@users.noreply.github.com>
2019-03-04 20:43:36 +0800

    Update q07_miles_to_kilometres.py

--- p02/q07_miles_to_kilometres.py
+++ p02/q07_miles_to_kilometres.py
@@ -1,1 +1,3 @@
-
+print("{0:10}{1:20}{2:10}   {3}".format("Miles","Kilometres","Kilometres","Miles"))
+for i in range (1,11):
+    print("{0:<10}{1:<20}{2:<10}   {3:<}".format(i,"{0:.3f}".format(i*1.609),i*5+15,"{0:.3f}".format((i*5+15)/1.609)))
