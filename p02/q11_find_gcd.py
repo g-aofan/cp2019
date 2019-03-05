8d9625d50b2127b78cce64b3d380d5677b9519a9
g-aofan <47775231+g-aofan@users.noreply.github.com>
2019-03-05 16:08:51 +0800

    Update q11_find_gcd.py

--- p02/q11_find_gcd.py
+++ p02/q11_find_gcd.py
@@ -1,1 +1,17 @@
+n1 = int(input("Enter first integer: "))
+n2 = int(input("Enter second integer: "))
 
+d = min([n1,n2])
+
+while True:
+    if n1 % d == 0 and n2 % d == 0:
+        print(d)
+        break
+    elif d == 0:
+        print("Error!")
+        break
+    d -= 1
+    
+#alternative solution
+#from math import gcd
+#print(gcd(30,4))
