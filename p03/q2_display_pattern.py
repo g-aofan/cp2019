7373291b4ac91bc1ae7fc158d4493a0eb3a4faff
g-aofan <47775231+g-aofan@users.noreply.github.com>
2019-03-06 11:23:17 +0800

    Update q2_display_pattern.py

--- p03/q2_display_pattern.py
+++ p03/q2_display_pattern.py
@@ -1,1 +1,8 @@
-
+n = int(input("Enter n: "))
+for row in range(1, n+1):
+    #print decreasing number of space
+    print(' '*(n-row), end="")
+    #print increasing number of digits
+    for col in range(row,0,-1):
+        print(col,end ="")
+    print()
